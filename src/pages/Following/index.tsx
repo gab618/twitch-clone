8be200e05a0750838f57d46fbea48598b3038bcd
Gabriel <knuckles4x@gmail.com>
2020-11-18 02:39:13 -0300

    Component: Header

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Text } from "react-native";
+import Header from "../../components/Header";
 
 import { Wrapper, Container, Main } from "./styles";
 
@@ -8,6 +9,7 @@ const Following: React.FC = () => {
   return (
     <Wrapper>
       <Container>
+        <Header />
         <Main />
       </Container>
     </Wrapper>
