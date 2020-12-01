959685255e864a03a63641c9e85f9628992d22a5
Gabriel <knuckles4x@gmail.com>
2020-11-30 23:24:31 -0300

    Component: ChannelList

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -7,6 +7,7 @@ import Heading from "../../components/Heading";
 import Title from "../../components/Title";
 import CategoryList from "../../components/CategoryList";
 import StreamList from "../../components/StreamList";
+import ChannelList from "../../components/ChannelList";
 
 import { Wrapper, Container, Main } from "./styles";
 
@@ -40,13 +41,13 @@ const Following: React.FC = () => {
         render: () => <Title>Continue Watching</Title>,
         isTitle: true,
       },
-      { key: "C3", render: () => <View /> },
+      { key: "C3", render: () => <StreamList /> },
       {
         key: "OFFLINE_CHANNELS",
         render: () => <Title>Offline Channels</Title>,
         isTitle: true,
       },
-      { key: "C4", render: () => <View /> },
+      { key: "C4", render: () => <ChannelList /> },
     ];
 
     const indices: number[] = [];
